d75866439cfcc45d7fbf212214d118c00eb8bfb8
Navyasree1609-bit <2320030266@klh.edu.in>
2024-09-11 09:12:30 +0530

    Add files via upload

---
+++ Week 4/AuctionEvent.java
@@ -0,0 +1,24 @@
+package auction;
+
+public interface AuctionEvent {
+    void update(String message);
+    package auction;
+
+public abstract class AuctionProcessTemplate {
+
+    public final void conductAuction() {
+        announceAuction();
+        startBidding();
+        endBidding();
+        declareWinner();
+    }
+
+    protected abstract void announceAuction();
+
+    protected abstract void startBidding();
+
+    protected abstract void endBidding();
+
+    protected abstract void declareWinner();
+}
+}
